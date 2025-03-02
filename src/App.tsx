a7f9bb8b394d10ec0a741e3b22c3cd48539c4e37
Monster Maker <dmonstermaker@gmail.com>
2025-03-02 12:58:47 +0000

    Update links

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ export default function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="./">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="./about">About</Link>
             </li>
           </ul>
         </nav>
